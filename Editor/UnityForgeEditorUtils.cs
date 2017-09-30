ee75ad91e32d5b61723419c71b616c5added76a6
Roman Fadeev <roma.fadeev@gmail.com>
2017-09-30 12:37:47 +0800

    Implement resources path editor field

--- Editor/UnityForgeEditorUtils.cs
+++ Editor/UnityForgeEditorUtils.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using UnityEditor;
 using UnityEngine;
 
@@ -8,6 +9,7 @@ namespace UnityForgeEditor
     public static class UnityForgeEditorUtils
     {
         private const float LIST_FIELD_REMOVE_BUTTON_WIDTH = 20.0f;
+        private const string RESOURCES_FOLDER_PATH = "Resources/";
 
         public static void ListField<T>(string title, IList<T> items, Func<T> itemCreator, Func<T, T> itemDrawer)
         {
@@ -50,6 +52,38 @@ namespace UnityForgeEditor
                 }
             }
             EditorGUILayout.EndVertical();
+        }
+
+        public static string ResourcesPathField<T>(string label, string path) where T : UnityEngine.Object
+        {
+            var result = path;
+            var obj = Resources.Load(path);
+
+            EditorGUI.BeginChangeCheck();
+
+            var newObj = EditorGUILayout.ObjectField(label, obj, typeof(T), false) as T;
+
+            if (EditorGUI.EndChangeCheck())
+            {
+                // For "None" object picker option return empty string as resources path
+                if (newObj == null)
+                {
+                    result = String.Empty;
+                }
+                else
+                {
+                    var newPath = AssetDatabase.GetAssetPath(newObj);
+                    if (newPath.Contains(RESOURCES_FOLDER_PATH))
+                    {
+                        // For paths containing several Resources folders like "Assets/A/Resources/C/D/E/Resources/F/G/Resources/File.extension"
+                        // save full resources path "C/D/E/Resources/F/G/Resources/File"
+                        var startIndex = newPath.IndexOf(RESOURCES_FOLDER_PATH) + RESOURCES_FOLDER_PATH.Length;
+                        result = newPath.Substring(startIndex).Replace(Path.GetExtension(newPath), String.Empty);
+                    }
+                }
+            }
+
+            return result;
         }
     }
 }
